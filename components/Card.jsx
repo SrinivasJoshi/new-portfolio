c05e1a3afea9bc02ae02192672b9ac5ed4759820
Srinivas Joshi <srinivasjoshi60@gmail.com>
2023-01-16 10:54:57 +0530

    images added

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,16 +4,30 @@ const Card = (props) => {
 	return (
 		<div className='border border-wwhite rounded-lg flex flex-col items-center'>
 			<img src={props.image} alt='Project Image' className='rounded-lg' />
-			<h3 className='text-lg font-kalam my-3'>{props.name}</h3>
-			<div className='flex justify-evenly items-center my-4'>
-				<a href={props.github_url}>
-					<img src='/GITHUB_ICON' alt='Github Icon' />
-				</a>
-				<a href={props.site_url}>
-					<img src='/=SITE_URL_IMAGE' alt='Github Icon' />
-				</a>
+			<h3 className='text-xl font-bold font-kalam my-3'>{props.name}</h3>
+			<div className='flex justify-between items-center w-3/5 mb-3'>
+				<p className='border border-wwhite px-2 py-1 rounded-xl text-xs'>
+					{props.tag}
+				</p>
+				<div className='flex justify-evenly items-center'>
+					<a href={props.github_url}>
+						<img
+							src='/images/github-logo.png'
+							alt='Github Icon'
+							className='w-8 h-8'
+						/>
+					</a>
+					<a href={props.site_url}>
+						<img
+							src='/images/link-logo.png'
+							alt='link to website'
+							className='w-8 h-8'
+						/>
+					</a>
+				</div>
 			</div>
-			<p className='text-center px-3 mb-2'>{props.description}</p>
+
+			<p className='text-center px-3 mb-4'>{props.description}</p>
 		</div>
 	);
 };
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -6,7 +6,8 @@ const Projects = () => {
 		{
 			id: 1,
 			name: 'Music NFTs',
-			image: '/images/icons8-done.gif',
+			image: '/images/project-images/2.png',
+			tag: '#WEB3',
 			description:
 				'Discover music-based NFTs with our marketplace! Destination for artists and music fans to buy, sell, and listen to music.',
 			github_url: 'some_url',
@@ -15,7 +16,8 @@ const Projects = () => {
 		{
 			id: 2,
 			name: 'Cosmo NFTs',
-			image: '/images/icons8-done.gif',
+			image: '/images/project-images/1.png',
+			tag: '#WEB3',
 			description:
 				'Get your hands on a limited edition, animated NFT based on your zodiac sign! Each NFT features a unique animation',
 			github_url: 'some_url',
@@ -23,47 +25,48 @@ const Projects = () => {
 		},
 		{
 			id: 3,
-			name: 'Music NFTs',
-			image: '/images/icons8-done.gif',
-			description:
-				'Experience the excitement of a card duel game that is based on the unique attributes of various characters by NFTs',
-			github_url: 'some_url',
-			site_url: 'some_site_url',
-		},
-	];
-
-	const web2Projects = [
-		{
-			id: 1,
-			name: 'Sorting Visualiser',
-			image: '/images/icons8-done.gif',
-			description:
-				'A tool that allows users to visualise and understand how different sorting algorithms work.Built using vanilla JS.',
-			github_url: 'some_url',
-			site_url: 'some_site_url',
-		},
-		{
-			id: 2,
 			name: 'Lose The “Very”',
-			image: '/images/icons8-done.gif',
+			image: '/images/project-images/3.png',
+			tag: '#AI',
 			description:
 				'Spice up your vocabulary and avoid overusing the word "very."Use of OpenAI’s API along with react.js for frontend.',
 			github_url: 'some_url',
 			site_url: 'some_site_url',
 		},
 		{
-			id: 3,
-			name: 'Twitter Clone(Twitt3r)',
-			image: '/images/icons8-done.gif',
+			id: 4,
+			name: 'Sorting Visualiser',
+			image: '/images/project-images/4.png',
+			tag: '#DSA',
 			description:
-				'Discover music-based NFTs with our marketplace! Destination for artists and music fans to buy, sell, and listen to music.',
+				'A tool that allows users to visualise and understand how different sorting algorithms work.Built using vanilla JS.',
+			github_url: 'some_url',
+			site_url: 'some_site_url',
+		},
+		{
+			id: 5,
+			name: 'Guess the color',
+			image: '/images/project-images/5.png',
+			tag: '#WEB2',
+			description:
+				'This is one of my first JS project.Users are presented with a RGB value along with colored tiles,they must match the square with the correct RGB value.',
+			github_url: 'some_url',
+			site_url: 'some_site_url',
+		},
+		{
+			id: 6,
+			name: 'Time Lock Contract',
+			image: '/images/project-images/6.png',
+			tag: '#WEB3',
+			description:
+				'A decentralized application that allows users to lock up their MATIC for a certain period of time. The app uses smart contracts enforce the time lock.',
 			github_url: 'some_url',
 			site_url: 'some_site_url',
 		},
 	];
 	return (
 		<section className='mt-10 flex flex-col justify-evenly min-h-screen items-center'>
-			<div className='flex flex-col justify-evenly items-center'>
+			<div className='flex flex-col justify-evenly items-center mb-10'>
 				<img
 					src='/images/undraw_code.svg'
 					alt='Person Image'
@@ -72,8 +75,7 @@ const Projects = () => {
 				<h2 className='text-2xl font-kalam'>My Projects</h2>
 			</div>
 
-			<h2 className='font-kalam text-lg'>Web3 projects</h2>
-			<div className='grid grid-cols-3 gap-6 w-4/5'>
+			<div className='grid grid-cols-3 gap-8 w-4/5'>
 				{web3Projects.map((project) => {
 					return (
 						<Card
@@ -83,21 +85,7 @@ const Projects = () => {
 							github_url={project.github_url}
 							site_url={project.site_url}
 							key={project.id}
-						/>
-					);
-				})}
-			</div>
-			<h2 className='font-kalam text-lg'>Web2 projects</h2>
-			<div className='grid grid-cols-3 gap-6 w-4/5'>
-				{web2Projects.map((project) => {
-					return (
-						<Card
-							name={project.name}
-							image={project.image}
-							description={project.description}
-							github_url={project.github_url}
-							site_url={project.site_url}
-							key={project.id}
+							tag={project.tag}
 						/>
 					);
 				})}
